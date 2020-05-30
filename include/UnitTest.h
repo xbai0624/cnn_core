d4b1ff14cd88122decd0593270a1d79f252f8a07
Xinzhan Bai <xb4zp@virginia.edu>
2020-05-30 03:08:18 -0400

    finish test vectorization & tensorization

---
+++ include/UnitTest.h
@@ -0,0 +1,23 @@
+#ifndef UNIT_TESTING_H
+#define UNIT_TESTING_H
+
+
+// an example playground, for testing different units and also for examples
+
+class UnitTest
+{
+public:
+    UnitTest();
+    ~UnitTest();
+
+    void Test();
+
+    void TestImagesStruct();
+
+private:
+    // reserved
+};
+
+
+
+#endif
---
+++ src/UnitTest.cpp
@@ -0,0 +1,58 @@
+#include "UnitTest.h"
+
+#include <iostream>
+
+#include "Layer.h" // test layer
+
+using namespace std;
+
+UnitTest::UnitTest()
+{
+    // reserved
+}
+
+UnitTest::~UnitTest()
+{
+    // reserved
+}
+
+void UnitTest::Test()
+{
+    TestImagesStruct();
+}
+
+
+void UnitTest::TestImagesStruct()
+{
+    // test Images struct in Layer.h file
+
+    // 1) empty test
+    Images __images;
+    //Images v_image0 = __images.Vectorization();
+
+    // 2) vectorization functionality test
+    cout<<"vectorization test"<<endl;
+    for(int i=0;i<4;i++)
+    {
+        Matrix kernel(3,4);
+	kernel.Random(); // fill matrix with random numbers
+	__images.OutputImageFromKernel.push_back(kernel);
+    }
+    for(auto &i: __images.OutputImageFromKernel)
+        cout<<i<<endl<<endl;
+
+    Images v_image = __images.Vectorization();
+    for(auto &i: v_image.OutputImageFromKernel)
+        cout<<i<<endl<<endl;
+
+    // 3) dimension not match test
+    //Matrix k(2, 4, 0);
+    //__images.OutputImageFromKernel.push_back(k);
+    //v_image = __images.Vectorization();
+
+    // 4) tensorization test
+    cout<<"Tensorization test"<<endl;
+    Images tensor_image = v_image.Tensorization(3, 4);
+    for(auto &i: tensor_image.OutputImageFromKernel)
+        cout<<i<<endl<<endl;
+}
